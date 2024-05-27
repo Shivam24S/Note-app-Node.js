d575984b6b8693002c7569827564f0004760a2a9
Shivam24S <shivamshrivastav8055@gmail.com>
2024-05-27 18:20:54 +0530

    chalk module practice

--- app.js
+++ app.js
@@ -27,6 +27,14 @@
 
 // importing file with es 6 js
 
-import getNotes from "./notes.js";
+// import getNotes from "./notes.js";
 
-console.log(getNotes());
+// console.log(getNotes());
+
+// practice session usage of chalk module
+
+import chalk from "chalk";
+
+console.log(chalk.green("success"));
+
+console.log(chalk.green.bgGray("Success!"));
