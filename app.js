fb20574b1ddcba1fcae2158500baf5a4010fd6d7
Shivam24S <shivamshrivastav8055@gmail.com>
2024-05-29 19:02:07 +0530

    yargv module related example

--- app.js
+++ app.js
@@ -33,18 +33,55 @@
 
 // practice session usage of chalk module
 
-import chalk from "chalk";
+// import chalk from "chalk";
 
-console.log(chalk.green("success"));
+// console.log(chalk.green("success"));
 
-console.log(chalk.green.bgGray("Success!"));
+// console.log(chalk.green.bgGray("Success!"));
 
 // getting user input using process.argv[] and command line
 
-console.log("argv", process.argv[2]);
+// console.log("argv", process.argv[2]);
 
-if (process.argv[2] === "add") {
-  console.log("adding Note");
-} else if (process.argv[2] === "remove") {
-  console.log("removing Note");
-}
+// if (process.argv[2] === "add") {
+//   console.log("adding Note");
+// } else if (process.argv[2] === "remove") {
+//   console.log("removing Note");
+// }
+
+// yargs module
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+yargs(hideBin(process.argv)).command({
+  command: "add",
+  describe: "adding Note",
+  handler: function () {
+    console.log("adding note ...");
+  },
+}).argv;
+
+yargs(hideBin(process.argv)).command({
+  command: "remove",
+  describe: "removing Note",
+  handler: function () {
+    console.log("removing note ...");
+  },
+}).argv;
+
+yargs(hideBin(process.argv)).command({
+  command: "list",
+  describe: "list note",
+  handler: function () {
+    console.log("listing notes ...");
+  },
+}).argv;
+
+yargs(hideBin(process.argv)).command({
+  command: "read",
+  describe: "read note",
+  handler: function () {
+    console.log("reading note ...");
+  },
+}).argv;
